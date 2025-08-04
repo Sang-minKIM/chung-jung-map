9200fcc0f4ae1baaeb91341f1e61043a25d166bd
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-04 23:43:54 +0900

    feat: tanstackRouter 사용 위한 vite config 변경
    - tanstackRouter의 routesDirectory를 './src/pages'로 변경
    - autoCodeSplitting 및 target 속성을 추가
    - App.tsx 파일을 삭제

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackRouter } from "@tanstack/router-vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { tanstackRouter } from '@tanstack/router-vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react({
-            jsxImportSource: "@emotion/react",
-            plugins: [
-                [
-                    "@swc/plugin-emotion",
-                    {
-                        sourceMap: true,
-                        autoLabel: "dev-only",
-                        labelFormat: "[local]",
-                    },
-                ],
-            ],
-        }),
-        tsconfigPaths(),
-        tanstackRouter({
-            routesDirectory: "./src/routes",
-            generatedRouteTree: "./src/routeTree.gen.ts",
-        }),
-    ],
-});
+  plugins: [
+    tanstackRouter({
+      routesDirectory: './src/pages',
+      generatedRouteTree: './src/routeTree.gen.ts',
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react({
+      jsxImportSource: '@emotion/react',
+      plugins: [
+        [
+          '@swc/plugin-emotion',
+          {
+            sourceMap: true,
+            autoLabel: 'dev-only',
+            labelFormat: '[local]',
+          },
+        ],
+      ],
+    }),
+    tsconfigPaths(),
+  ],
+})
