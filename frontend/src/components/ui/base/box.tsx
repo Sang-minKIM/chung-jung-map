b326650eea7268770dc9626a12b33b8aa0a0d877
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-09 20:12:03 +0900

    refactor: Box 컴포넌트 리팩토링 및 스타일 훅 적용
    - style-props 인터페이스 변경에 의한 수정
    - BoxProps 인터페이스에 children 및 className 속성 추가
    - styled-components 대신 css prop을 사용하여 스타일 적용

--- frontend/src/components/ui/base/box.tsx
+++ frontend/src/components/ui/base/box.tsx
@@ -1,21 +1,86 @@
-import styled from '@emotion/styled'
+import React from 'react'
+import { css } from '@emotion/react'
 import {
-  space,
-  layout,
-  color,
-  typography,
-  flexItem,
-  gridItem,
-  type GridItemProps,
-  type FlexItemProps,
+  useSpaceStyles,
+  useLayoutStyles,
+  useColorStyles,
+  useTypographyStyles,
+  useFlexItemStyles,
+  useGridItemStyles,
   type SpaceProps,
   type LayoutProps,
   type ColorProps,
   type TypographyProps,
+  type FlexItemProps,
+  type GridItemProps,
 } from '~/styles/style-props'
 
 export interface BoxProps extends SpaceProps, LayoutProps, ColorProps, TypographyProps, FlexItemProps, GridItemProps {
   as?: React.ElementType
+  children?: React.ReactNode
+  className?: string
 }
 
-export const Box = styled.div<BoxProps>(space, layout, color, typography, flexItem, gridItem)
+export const Box = ({ as: Component = 'div', children, className, ...rest }: BoxProps) => {
+  const {
+    m,
+    mx,
+    my,
+    mt,
+    mr,
+    mb,
+    ml,
+    p,
+    px,
+    py,
+    pt,
+    pr,
+    pb,
+    pl,
+    width,
+    height,
+    minWidth,
+    minHeight,
+    maxWidth,
+    maxHeight,
+    overflow,
+    color,
+    backgroundColor,
+    fontSize,
+    fontWeight,
+    textAlign,
+    flex,
+    flexBasis,
+    flexGrow,
+    flexShrink,
+    alignSelf,
+    gridArea,
+    gridColumn,
+    gridRow,
+    justifySelf,
+    ...restProps
+  } = rest
+  const spaceStyles = useSpaceStyles({ m, mx, my, mt, mr, mb, ml, p, px, py, pt, pr, pb, pl })
+  const layoutStyles = useLayoutStyles({ width, height, minWidth, minHeight, maxWidth, maxHeight, overflow })
+  const colorStyles = useColorStyles({ color, backgroundColor })
+  const typographyStyles = useTypographyStyles({ fontSize, fontWeight, textAlign })
+  const flexItemStyles = useFlexItemStyles({ flex, flexBasis, flexGrow, flexShrink, alignSelf })
+  const gridItemStyles = useGridItemStyles({ alignSelf, gridArea, gridColumn, gridRow, justifySelf })
+
+  const combinedStyles = {
+    ...spaceStyles,
+    ...layoutStyles,
+    ...colorStyles,
+    ...typographyStyles,
+    ...flexItemStyles,
+    ...gridItemStyles,
+  }
+
+  return (
+    <Component css={css(combinedStyles)} className={className} {...restProps}>
+      {children}
+    </Component>
+  )
+}
+
+Box.displayName = 'Box'
