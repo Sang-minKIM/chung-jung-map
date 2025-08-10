370fb4f993ea66a0b381f8b3a0f650567de14528
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-10 13:44:00 +0900

    style: 서비스 레이아웃 스타일 추가

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,26 +1,58 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { Flex } from '~/components/layout/flex'
+import { Text } from '~/components/typo/text'
+import { Card } from '~/components/ui/card'
+import { Dot } from '~/components/ui/dot'
+import styled from '@emotion/styled'
+import { Link } from '~/components/ui/link'
+import { Heading } from '~/components/typo/heading'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          홈
+    <Container>
+      <Flex as="header" justify="between" align="center">
+        <Link to="/">
+          <Heading as="h2" fontSize="2xl">
+            Logo
+          </Heading>
         </Link>
-        <Link to="/about" className="[&.active]:font-bold">
-          서비스 소개
-        </Link>
-        <Link to="/policies" className="[&.active]:font-bold">
-          정책 안내
-        </Link>
-        <Link to="/notices" className="[&.active]:font-bold">
-          공고 목록
-        </Link>
-      </div>
+        <Flex as="nav" gap="md">
+          <Link to="/policies">정책 안내</Link>
+          <Link to="/notices">공고 목록</Link>
+        </Flex>
+      </Flex>
       <hr />
+      <Flex gap="md" justify="center" align="center" height="500px">
+        <Card variant="basic" height="100px" width="100px">
+          <Text>
+            <Dot color="red" size="lg" />
+            Basic
+          </Text>
+        </Card>
+        <Card variant="surface" height="100px" width="100px">
+          <Text>Surface</Text>
+        </Card>
+        <Card variant="accent" height="100px" width="100px">
+          <Text>Accent</Text>
+        </Card>
+      </Flex>
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </Container>
   ),
 })
+
+export const Container = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 0 ${({ theme }) => theme.space.lg};
+
+  @media (min-width: 640px) {
+    padding: 0 ${({ theme }) => theme.space.xl};
+  }
+
+  @media (min-width: 1024px) {
+    padding: 0 ${({ theme }) => theme.space['2xl']};
+  }
+`
