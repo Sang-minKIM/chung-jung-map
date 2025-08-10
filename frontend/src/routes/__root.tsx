0b46c537ae1d8ffd667358f1312de9f897193799
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-10 18:12:05 +0900

    feat: root layout에 header, footer 추가

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,30 +1,14 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { Flex } from '~/components/layout/flex'
-import { Text } from '~/components/typo/text'
-import { Card } from '~/components/ui/card'
-import { Dot } from '~/components/ui/dot'
 import { Header } from '~/components/ui/header'
+import { Footer } from '~/components/ui/footer'
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Header />
-      <Flex gap="md" justify="center" align="center" height="500px">
-        <Card variant="basic" height="100px" width="100px">
-          <Text>
-            <Dot color="red" size="lg" />
-            Basic
-          </Text>
-        </Card>
-        <Card variant="surface" height="100px" width="100px">
-          <Text>Surface</Text>
-        </Card>
-        <Card variant="accent" height="100px" width="100px">
-          <Text>Accent</Text>
-        </Card>
-      </Flex>
       <Outlet />
+      <Footer />
       <TanStackRouterDevtools />
     </>
   ),
