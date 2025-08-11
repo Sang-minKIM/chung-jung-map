42b4ccd1f0a3004a4f98a99f379cd66de026da40
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-11 23:58:35 +0900

    feat: 정책 리스트 컴포넌트 추가
    - queries layer에서부터 routes까지의 api 호출 방식 패턴 정의

--- frontend/src/routes/policies/index.tsx
+++ frontend/src/routes/policies/index.tsx
@@ -1,17 +1,30 @@
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import { z } from 'zod'
-
-const policySearchSchema = z.object({
-  keyword: z.string().optional(),
-  category: z.enum(['주거', '금융', '일자리', '창업', '교육', '복지']).optional(),
-  region: z.string().optional(),
-  target: z.enum(['청년', '대학생', '구직자', '창업자']).optional(),
-  status: z.enum(['진행중', '마감', '예정']).optional(),
-  page: z.coerce.number().min(1).default(1),
-  limit: z.coerce.number().min(10).max(50).default(20),
-})
+import { getPoliciesQueryOptions } from '~/queries/policies/policies.query'
+import { PoliciesSearchSchema } from '~/queries/policies/policies.type'
+import { queryClient } from '~/queries/query-client'
 
 export const Route = createFileRoute('/policies/')({
-  validateSearch: policySearchSchema,
-  component: () => <div>정책 안내</div>,
+  validateSearch: PoliciesSearchSchema,
+  loaderDeps: ({ search }) => ({ search }),
+  loader: ({ deps }) => queryClient.ensureQueryData(getPoliciesQueryOptions(deps.search)),
+  component: () => <Policies />,
 })
+
+function Policies() {
+  const search = Route.useSearch()
+
+  const {
+    data: { data: policies },
+  } = useSuspenseQuery(getPoliciesQueryOptions(search))
+
+  console.log(policies)
+
+  return (
+    <div>
+      {policies?.map((policy) => (
+        <div key={policy.id}>{policy.title}</div>
+      ))}
+    </div>
+  )
+}
