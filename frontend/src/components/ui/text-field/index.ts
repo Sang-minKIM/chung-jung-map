29361330209f9914e179deaa10df9de45f75f250
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-19 22:23:39 +0900

    refactor: text-field 컴포넌트 파일명 변경

---
+++ frontend/src/components/ui/text-field/index.ts
@@ -0,0 +1,13 @@
+import { TextFieldRoot } from './text-field-root'
+import { TextFieldInput } from './text-field-input'
+import { TextFieldSlot } from './text-field-slot'
+
+export const TextField = {
+  Root: TextFieldRoot,
+  Input: TextFieldInput,
+  Slot: TextFieldSlot,
+}
+
+export type { TextFieldRootProps } from './text-field-root'
+export type { TextFieldInputProps } from './text-field-input'
+export type { TextFieldSlotProps } from './text-field-slot'
