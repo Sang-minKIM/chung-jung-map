cf02cea6bda56a0523cbb0010bb197ec08bb3712
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-30 14:55:01 +0900

    chore(FE): 공고 쿼리 옵션의 response type에Prettify 사용

--- frontend/src/queries/notices/notices.query.ts
+++ frontend/src/queries/notices/notices.query.ts
@@ -2,9 +2,10 @@ import { queryOptions } from '@tanstack/react-query'
 import { NOTICES_ENDPOINT, NOTICES_KEY } from './notices.model'
 import { request } from '../request'
 import type { NoticesResponse, NoticesSearchParams } from './notices.type'
+import type { Prettify } from '~/types/prettify'
 
 export const getNoticesQueryOptions = (params?: NoticesSearchParams) =>
-  queryOptions<NoticesResponse>({
+  queryOptions<Prettify<NoticesResponse>>({
     queryKey: NOTICES_KEY.list(params),
     queryFn: () => request(NOTICES_ENDPOINT.list(), { params }),
   })
