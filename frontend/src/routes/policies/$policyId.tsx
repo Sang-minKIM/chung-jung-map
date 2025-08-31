779cf393cca3165b630a83b38dbd81e6c100254b
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-31 23:41:01 +0900

    chore(FE): 미사용 코드 제거

--- frontend/src/routes/policies/$policyId.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { z } from 'zod'
-
-export const Route = createFileRoute('/policies/$policyId')({
-  parseParams: ({ policyId }) => ({
-    policyId: z.string().min(1).parse(policyId),
-  }),
-  component: () => <div>정책 상세 설명</div>,
-})
