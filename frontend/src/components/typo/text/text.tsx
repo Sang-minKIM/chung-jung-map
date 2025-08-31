ff625022d1e5baed83a2999863dfd98c912bba8e
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-31 23:41:12 +0900

    feat(FE): Text 컴포넌트에 'pre' 타입 추가 및 스타일 적용

--- frontend/src/components/typo/text/text.tsx
+++ frontend/src/components/typo/text/text.tsx
@@ -1,15 +1,22 @@
 import { Box, type BoxProps } from '~/components/layout/box'
 import type { ReactNode } from 'react'
+import { css } from '@emotion/react'
 
 export interface TextProps extends BoxProps {
   children?: ReactNode
-  as?: 'p' | 'span'
+  as?: 'p' | 'span' | 'pre'
 }
 
 export function Text({ as = 'span', fontSize = 'md', color = 'inherit', m = 'none', children, ...rest }: TextProps) {
   return (
-    <Box as={as} fontSize={fontSize} color={color} m={m} {...rest}>
+    <Box as={as} fontSize={fontSize} color={color} m={m} css={as === 'pre' ? preStyles : undefined} {...rest}>
       {children}
     </Box>
   )
 }
+
+const preStyles = css`
+  line-height: 1.6;
+  white-space: pre-wrap;
+  word-break: keep-all;
+`
