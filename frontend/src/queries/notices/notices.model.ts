edcb812e4371c780952ea9f53e0bd0955c0f0a83
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-31 15:28:25 +0900

    feat(FE): 공고 상세 조회 쿼리 추가

--- frontend/src/queries/notices/notices.model.ts
+++ frontend/src/queries/notices/notices.model.ts
@@ -3,9 +3,11 @@ import type { NoticesSearchParams } from './notices.type'
 export const NOTICES_ENDPOINT = {
   default: '/notices',
   list: () => NOTICES_ENDPOINT.default,
+  detail: (id: string) => `${NOTICES_ENDPOINT.default}/${id}`,
 }
 
 export const NOTICES_KEY = {
   default: ['notices'],
   list: (params?: NoticesSearchParams) => [...NOTICES_KEY.default, 'list', params],
+  detail: (id: string) => [...NOTICES_KEY.default, 'detail', id],
 }
--- frontend/src/queries/notices/notices.query.ts
+++ frontend/src/queries/notices/notices.query.ts
@@ -1,11 +1,17 @@
 import { queryOptions } from '@tanstack/react-query'
 import { NOTICES_ENDPOINT, NOTICES_KEY } from './notices.model'
 import { request } from '../request'
-import type { NoticesResponse, NoticesSearchParams } from './notices.type'
+import type { NoticesResponse, NoticesSearchParams, Notice } from './notices.type'
 import type { Prettify } from '~/types/prettify'
 
 export const getNoticesQueryOptions = (params?: NoticesSearchParams) =>
   queryOptions<Prettify<NoticesResponse>>({
     queryKey: NOTICES_KEY.list(params),
     queryFn: () => request(NOTICES_ENDPOINT.list(), { params }),
+  })
+
+export const getNoticeDetailQueryOptions = (id: string) =>
+  queryOptions<{ data: Prettify<Notice> }>({
+    queryKey: NOTICES_KEY.detail(id),
+    queryFn: () => request(NOTICES_ENDPOINT.detail(id)),
   })
