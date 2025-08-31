5a15e217bfcdb1ea38c82ee70c247175019df94a
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-08-31 13:31:50 +0900

    feat(FE): 공고 결과 섹션에 페이지네이션 기능 추가

--- frontend/src/routes/notices/-components/list/notice-results-section.tsx
+++ frontend/src/routes/notices/-components/list/notice-results-section.tsx
@@ -6,12 +6,24 @@ import { Grid } from '~/components/layout/grid'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { getNoticesQueryOptions } from '~/queries/notices/notices.query'
 import { NoticeCard } from './notice-card'
+import { Pagination } from '~/components/ui/pagination'
+import { useNavigate } from '@tanstack/react-router'
 
 export function NoticeResultsSection() {
   const search = Route.useSearch()
+  const navigate = useNavigate({ from: Route.fullPath })
   const {
     data: { data: notices, policyInfo, pagination },
   } = useSuspenseQuery(getNoticesQueryOptions(search))
+
+  const onPageChange = (page: number) => {
+    navigate({
+      search: (prev) => ({
+        ...prev,
+        page,
+      }),
+    })
+  }
 
   return (
     <Box as="section" py="xl">
@@ -24,6 +36,12 @@ export function NoticeResultsSection() {
           <NoticeCard key={notice.id} notice={notice} />
         ))}
       </Grid>
+      <Pagination
+        totalItems={pagination.totalCount}
+        itemsPerPage={search.limit}
+        currentPage={search.page}
+        onPageChange={onPageChange}
+      />
     </Box>
   )
 }
