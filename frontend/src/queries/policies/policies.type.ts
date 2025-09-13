2c19693237fed040d20758550d7eda24cca7d351
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-09-13 14:51:57 +0900

    fix(FE): 정책 타입에 subCategory 추가 및 정책 리스트 data 타입을 필요한 것만 pick 하도록 변경

--- frontend/src/queries/policies/policies.type.ts
+++ frontend/src/queries/policies/policies.type.ts
@@ -2,13 +2,14 @@ type Policy = {
   id: number
   title: string
   category: '금융' | '주거' | '일자리' | '교육' | '생활복지문화' | '참여'
+  subCategory: string
   targetGroup: string
   description: string
 }
 
 export type PoliciesResponse = {
   success: boolean
-  data: Policy[]
+  data: Pick<Policy, 'id' | 'title' | 'category' | 'description'>[]
   total: number
   message: string
 }
