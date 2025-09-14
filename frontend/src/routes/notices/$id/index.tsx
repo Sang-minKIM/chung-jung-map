f576491ace6e207c93a20df995d7b61fc1a2e29b
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-09-14 12:49:12 +0900

    feat(FE): refactor(FE): 공고 상세 페이지 리팩토링
    - 공고 상세 정보를 InfoCard와 DataList 컴포넌트를 사용하여 추상화 단계를 일관성있게 유지
    - 시점 이동이 일어나지 않게 객체 배열을 제거하고 JSX에 선언적으로 표현
    - data의 null, empty 여부에 따른 렌더링 로직을 드러냄.

--- frontend/src/routes/notices/$id/index.tsx
+++ frontend/src/routes/notices/$id/index.tsx
@@ -1,19 +1,17 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import { Flex } from '~/components/layout/flex'
-import { Heading } from '~/components/typo/heading'
 import { getNoticeDetailQueryOptions } from '~/routes/notices/-queries/notices.query'
 import { NOTICE_CATEGORY_OPTIONS } from '../-constants/notice-category-options'
 import { Text } from '~/components/typo/text'
 import { Tag } from '~/components/ui/tag'
-import { Card } from '~/components/ui/card'
-import { Building2, Calendar, ExternalLink, type LucideIcon } from 'lucide-react'
+import { Building2, Calendar, ExternalLink } from 'lucide-react'
 import { Link } from '~/components/ui/link'
-import { useTheme } from '@emotion/react'
 import { Separator } from '~/components/ui/separator'
-import { Box } from '~/components/layout/box'
-import { isNull, map, pipe, reject, toArray } from '@fxts/core'
 import { Separated } from '~/components/kits/separated'
+import type { Notice } from '../-queries/notices.type'
+import { InfoCard } from '~/components/ui/info-card'
+import { DataList } from '~/components/ui/data-list'
 
 export const Route = createFileRoute('/notices/$id/')({
   component: () => <NoticeDetail />,
@@ -28,125 +26,156 @@ function NoticeDetail() {
     data: { data: notice },
   } = useSuspenseQuery(getNoticeDetailQueryOptions(id))
 
-  const categoryMeta =
-    NOTICE_CATEGORY_OPTIONS.find((category) => category.value === notice.category) ??
-    NOTICE_CATEGORY_OPTIONS.find((category) => category.value === '기타')!
-
-  const coreDataList = [
-    {
-      label: '신청 기간',
-      value: notice.startDate && notice.endDate ? `${notice.startDate} ~ ${notice.endDate}` : null,
-      Icon: Calendar,
-      type: 'text',
-    },
-    { label: '신청 링크', value: notice.url, Icon: ExternalLink, type: 'link' },
-    { label: '관련 링크', value: notice.referenceUrl, Icon: ExternalLink, type: 'link' },
-    { label: '주관 기관', value: notice.supervisingInstitution, Icon: Building2, type: 'text' },
-    { label: '운영 기관', value: notice.operatingInstitution, Icon: Building2, type: 'text' },
-    { label: '등록 기관', value: notice.registeringInstitution, Icon: Building2, type: 'text' },
-  ]
-
-  const infoSectionDataList = [
-    { label: '개요', value: notice.description },
-    { label: '지원 혜택', value: notice.supportContent },
-    { label: '신청 절차', value: notice.applicationMethod },
-    { label: '심사 절차', value: notice.screeningMethod },
-    { label: '참고 사항', value: notice.additionalInfo },
-    { label: '제출 서류', value: notice.requiredDocuments },
-  ]
-
   return (
     <Flex as="main" direction="column" gap="xl">
-      <Flex as="ul" gap="md">
-        <Tag as="li" color={categoryMeta.color} size="sm">
-          {categoryMeta.label}
-        </Tag>
+      <NoticeTags category={notice.category} regionalInstitution={notice.regionalInstitution} />
+      <InfoCard.Root>
+        <InfoCard.Heading as="h1" size="lg">
+          {notice.title}
+        </InfoCard.Heading>
+        <InfoCard.Content variant="surface">
+          <DataList.Root>
+            <Separated by={<Separator size="full" />}>
+              {notice.startDate && notice.endDate && (
+                <DataList.Item>
+                  <DataList.Label Icon={Calendar}>신청 기간</DataList.Label>
+                  <DataList.Value>{`${notice.startDate} ~ ${notice.endDate}`}</DataList.Value>
+                </DataList.Item>
+              )}
 
-        {notice.regionalInstitution && (
-          <Tag color="blue" size="sm">
-            {notice.regionalInstitution}
-          </Tag>
-        )}
-      </Flex>
+              {notice.url && (
+                <DataList.Item>
+                  <DataList.Label Icon={ExternalLink}>신청 링크</DataList.Label>
+                  <DataList.Value>
+                    <Link href={notice.url} target="_blank">
+                      {notice.url}
+                    </Link>
+                  </DataList.Value>
+                </DataList.Item>
+              )}
 
-      <Heading as="h1" fontSize="lg">
-        {notice.title}
-      </Heading>
+              {notice.referenceUrl && (
+                <DataList.Item>
+                  <DataList.Label Icon={ExternalLink}>관련 링크</DataList.Label>
+                  <DataList.Value>
+                    <Link href={notice.referenceUrl} target="_blank">
+                      {notice.referenceUrl}
+                    </Link>
+                  </DataList.Value>
+                </DataList.Item>
+              )}
 
-      <Card p="xl" as="article" variant="surface">
-        <Flex direction="column" gap="lg">
-          <Separated by={<Separator size="full" />}>
-            {pipe(
-              coreDataList,
-              reject((data) => isNull(data.value)),
-              map((data) => {
-                if (data.type === 'text') {
-                  return <TextData label={data.label} value={data.value!} Icon={data.Icon} />
-                } else {
-                  return <LinkData label={data.label} href={data.value!} Icon={data.Icon} />
-                }
-              }),
-              toArray
-            )}
-          </Separated>
-        </Flex>
-      </Card>
+              {notice.supervisingInstitution && (
+                <DataList.Item>
+                  <DataList.Label Icon={Building2}>주관 기관</DataList.Label>
+                  <DataList.Value>{notice.supervisingInstitution}</DataList.Value>
+                </DataList.Item>
+              )}
 
-      {pipe(
-        infoSectionDataList,
-        reject((data) => isNull(data.value)),
-        map((data) => {
-          return <InfoSection label={data.label} value={data.value!} />
-        }),
-        toArray
+              {notice.supervisingInstitution && (
+                <DataList.Item>
+                  <DataList.Label Icon={Building2}>주관 기관</DataList.Label>
+                  <DataList.Value>{notice.supervisingInstitution}</DataList.Value>
+                </DataList.Item>
+              )}
+
+              {notice.operatingInstitution && (
+                <DataList.Item>
+                  <DataList.Label Icon={Building2}>운영 기관</DataList.Label>
+                  <DataList.Value>{notice.operatingInstitution}</DataList.Value>
+                </DataList.Item>
+              )}
+
+              {notice.registeringInstitution && (
+                <DataList.Item>
+                  <DataList.Label Icon={Building2}>등록 기관</DataList.Label>
+                  <DataList.Value>{notice.registeringInstitution}</DataList.Value>
+                </DataList.Item>
+              )}
+            </Separated>
+          </DataList.Root>
+        </InfoCard.Content>
+      </InfoCard.Root>
+
+      {notice.description && (
+        <InfoCard.Root>
+          <InfoCard.Heading>개요</InfoCard.Heading>
+          <InfoCard.Content>
+            <Text as="pre">{notice.description}</Text>
+          </InfoCard.Content>
+        </InfoCard.Root>
+      )}
+
+      {notice.supportContent && (
+        <InfoCard.Root>
+          <InfoCard.Heading>지원 혜택</InfoCard.Heading>
+          <InfoCard.Content>
+            <Text as="pre">{notice.supportContent}</Text>
+          </InfoCard.Content>
+        </InfoCard.Root>
+      )}
+
+      {notice.applicationMethod && (
+        <InfoCard.Root>
+          <InfoCard.Heading>신청 절차</InfoCard.Heading>
+          <InfoCard.Content>
+            <Text as="pre">{notice.applicationMethod}</Text>
+          </InfoCard.Content>
+        </InfoCard.Root>
+      )}
+
+      {notice.screeningMethod && (
+        <InfoCard.Root>
+          <InfoCard.Heading>심사 절차</InfoCard.Heading>
+          <InfoCard.Content>
+            <Text as="pre">{notice.screeningMethod}</Text>
+          </InfoCard.Content>
+        </InfoCard.Root>
+      )}
+
+      {notice.additionalInfo && (
+        <InfoCard.Root>
+          <InfoCard.Heading>참고 사항</InfoCard.Heading>
+          <InfoCard.Content>
+            <Text as="pre">{notice.additionalInfo}</Text>
+          </InfoCard.Content>
+        </InfoCard.Root>
+      )}
+
+      {notice.requiredDocuments && (
+        <InfoCard.Root>
+          <InfoCard.Heading>제출 서류</InfoCard.Heading>
+          <InfoCard.Content>
+            <Text as="pre">{notice.requiredDocuments}</Text>
+          </InfoCard.Content>
+        </InfoCard.Root>
       )}
     </Flex>
   )
 }
 
-function TextData({ label, value, Icon }: { label: string; value: string; Icon: LucideIcon }) {
-  const theme = useTheme()
+function NoticeTags({
+  category,
+  regionalInstitution,
+}: {
+  category: Notice['category']
+  regionalInstitution: Notice['regionalInstitution']
+}) {
+  const categoryOption =
+    NOTICE_CATEGORY_OPTIONS.find((option) => option.value === category) ??
+    NOTICE_CATEGORY_OPTIONS.find((option) => option.value === '기타')!
 
   return (
-    <Flex>
-      <Flex gap="sm" flex="1">
-        <Icon size={theme.componentSizes.md.iconSize} color={theme.colors.grey500} />
-        <Text color="grey500">{label}</Text>
-      </Flex>
-      <Text flex="3" fontWeight="medium">
-        {value}
-      </Text>
-    </Flex>
-  )
-}
+    <Flex as="ul" gap="md">
+      <Tag as="li" color={categoryOption.color} size="sm">
+        {categoryOption.label}
+      </Tag>
 
-function LinkData({ label, href, Icon }: { label: string; href: string; Icon: LucideIcon }) {
-  const theme = useTheme()
-
-  return (
-    <Flex>
-      <Flex gap="sm" flex="1">
-        <Icon size={theme.componentSizes.md.iconSize} color={theme.colors.grey500} />
-        <Text color="grey500">{label}</Text>
-      </Flex>
-      <Box flex="3">
-        <Link href={href} target="_blank">
-          {href}
-        </Link>
-      </Box>
-    </Flex>
-  )
-}
-
-function InfoSection({ value, label }: { value: string; label: string }) {
-  return (
-    <Flex as="section" direction="column" gap="sm">
-      <Heading as="h2" fontSize="md" fontWeight="semibold">
-        {label}
-      </Heading>
-      <Card p="xl" as="article">
-        <Text as="pre">{value}</Text>
-      </Card>
+      {regionalInstitution && (
+        <Tag color="blue" size="sm">
+          {regionalInstitution}
+        </Tag>
+      )}
     </Flex>
   )
 }
