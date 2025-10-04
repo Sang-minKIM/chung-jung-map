feacfe362c44b737d0edf8abc4b2014538dc00f7
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-10-04 11:58:24 +0900

    feat(FE): 랜딩 페이지 기본 레이아웃 추가

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,36 +1,39 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
-import styled from '@emotion/styled'
-import { Button } from '~/components/ui/button'
-
-const StyledTitle = styled.h1`
-  color: #2563eb;
-  font-size: 2rem;
-  font-weight: bold;
-  text-align: center;
-  margin: 2rem 0;
-`
+import { createFileRoute } from '@tanstack/react-router'
+import { Flex } from '~/components/layout/flex'
+import { Heading } from '~/components/typo/heading'
+import { Text } from '~/components/typo/text'
 
 export const Route = createFileRoute('/')({
   component: () => (
-    <div>
-      <StyledTitle>청년 정책 맵 홈페이지</StyledTitle>
-      <div
-        css={{
-          textAlign: 'center',
-          padding: '1rem',
-          backgroundColor: '#f8fafc',
-          borderRadius: '8px',
-          margin: '1rem',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '1rem',
-        }}
-      >
-        추후 랜딩 페이지 업데이트 예정입니다.
-        <Link to="/policies">
-          <Button>정책 페이지로 이동</Button>
-        </Link>
-      </div>
-    </div>
+    <>
+      <Flex direction="column" align="center" justify="center" gap="lg" height="100dvh">
+        <Heading as="h1" fontSize="2xl" fontWeight="bold">
+          청년 정책,
+        </Heading>
+        <Heading as="h1" fontSize="2xl" fontWeight="bold">
+          이제 놓치지 마세요
+        </Heading>
+      </Flex>
+      <Flex align="center" justify="center" gap="lg" height="100dvh">
+        <Flex direction="column" align="center" justify="center" gap="lg">
+          <Heading as="h2" fontSize="xl" fontWeight="bold">
+            정책 탐색
+          </Heading>
+          <Text as="p" fontSize="lg">
+            어려운 청년 정책, 60여 가지의 정책 개요를 통해 쉽게 탐색해보세요.
+          </Text>
+        </Flex>
+      </Flex>
+      <Flex align="center" justify="center" gap="lg" height="100dvh">
+        <Flex direction="column" align="center" justify="center" gap="lg">
+          <Heading as="h2" fontSize="xl" fontWeight="bold">
+            공고 탐색
+          </Heading>
+          <Text as="p" fontSize="lg">
+            정책과 관련된 공고를 청정맵 만의 유사도 측정 기술을 통해 추천해드려요.
+          </Text>
+        </Flex>
+      </Flex>
+    </>
   ),
 })
